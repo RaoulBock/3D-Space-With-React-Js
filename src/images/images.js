1f34d2baad4fc1c83ba402b7701e670f2b400c54
RaoulBock <anthonyraoulbock01@gmail.com>
2022-09-23 09:42:21 +0200

    Images and Textures

--- src/images/images.js
+++ src/images/images.js
@@ -3,5 +3,6 @@ import glassImg from "./glass.png";
 import grassImg from "./grass.jpg";
 import logImg from "./log.jpg";
 import woodImg from "./wood.png";
+import roadImg from "./road.png";
 
-export { dirtImg, glassImg, grassImg, logImg, woodImg };
+export { dirtImg, glassImg, grassImg, logImg, woodImg, roadImg };
--- src/images/textures.js
+++ src/images/textures.js
@@ -1,20 +1,34 @@
 import { NearestFilter, TextureLoader, RepeatWrapping } from "three";
 
-import { dirtImg, glassImg, grassImg, logImg, woodImg } from "./images";
+import {
+  dirtImg,
+  glassImg,
+  grassImg,
+  logImg,
+  woodImg,
+  roadImg
+} from "./images";
 
 const dirtTexture = new TextureLoader().load(dirtImg);
 const glassTexture = new TextureLoader().load(glassImg);
 const grassTexture = new TextureLoader().load(grassImg);
 const logTexture = new TextureLoader().load(logImg);
 const woodTexture = new TextureLoader().load(woodImg);
+const roadTexture = new TextureLoader().load(roadImg);
 
 const groundTexture = new TextureLoader().load(grassImg);
+const driveTexture = new TextureLoader().load(roadImg);
 
 dirtTexture.magFilter = NearestFilter;
 glassTexture.magFilter = NearestFilter;
 grassTexture.magFilter = NearestFilter;
 logTexture.magFilter = NearestFilter;
 woodTexture.magFilter = NearestFilter;
+roadTexture.magFilter = NearestFilter;
+
+driveTexture.wrapS = RepeatWrapping;
+driveTexture.wrapT = RepeatWrapping;
+driveTexture.repeat.set(4, 4);
 
 groundTexture.wrapS = RepeatWrapping; // load texture, set wrap mode to repeat
 groundTexture.wrapT = RepeatWrapping; // load texture, set wrap mode to repeat
